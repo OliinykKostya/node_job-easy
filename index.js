80112004eb26bd182f1e8ed56df1aa7d9c4c3f42
OliinykKostya <oliinyk.kostya@gmail.com>
2020-08-26 23:38:09 +0300

    test

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const cors = require('cors')
 app.use(cors())
 
 app.use((req, res, next) => {
-  res.set('Access-Control-Allow-Origin', 'https://oliinykkostya.github.io/')
+  res.set('Access-Control-Allow-Origin', 'https://oliinykkostya.github.io')
   // res.set('Access-Control-Allow-Origin', 'http://localhost:3000')
   res.set('Access-Control-Allow-Headers', 'content-type')
   res.set('Access-Control-Allow-Methods', 'POST')
